4c786839c0e6d38516d8c70defab178e96c3b79e
DropXpert <56500369+DropXpert@users.noreply.github.com>
2025-01-28 20:23:38 +0530

    Add files via upload

---
+++ utils/banner.js
@@ -0,0 +1,16 @@
+const banner = `
+\x1b[34m  
+ ____                __  __                _   
+|  _ \ _ __ ___  _ __\ \/ /_ __   ___ _ __| |_ 
+| | | | '__/ _ \| '_ \\  /| '_ \ / _ \ '__| __|
+| |_| | | | (_) | |_) /  \| |_) |  __/ |  | |_ 
+|____/|_|  \___/| .__/_/\_\ .__/ \___|_|   \__|
+                |_|       |_|                  
+
+*    This script is created just for my subscribers   *
+*    Do not sell or distribute it for profit   *
+
+         
+\x1b[0m`; // Reset color
+
+export default banner;
